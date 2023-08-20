75c16a9861752dcd8c10d99d0e789b332909ee2c
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-08-20 10:40:02 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ var bodyParser=require("body-parser")
 app.set("view engine","ejs")
 app.use(app1.static("public"))
 app.use(bodyParser.urlencoded({extended:false}))
-const PORT=process.env||3030
+const PORT=process.env.PORT||3030
 app.listen(PORT,function(req,res){
     console.log("here now")
 })
